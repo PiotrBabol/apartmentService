3df47e3a4357527075de881204aa776e7df9177e
P B <piotrbabol@outlook.com>
2018-08-05 14:29:53 +0200

    Edit to controller package: Added RestController and updated ApartmentController

---
+++ src/main/java/com/apartmentService/apartment/controller/ApartmentContorllerConstants.java
@@ -0,0 +1,15 @@
+package com.apartmentService.apartment.controller;
+
+class ApartmentContorllerConstants {
+    static final String DEFAULT_PATH = "/";
+    static final String DEFAULT_PAGE = "apartment";
+    static final String READ_APARTMENTS_PATH = "/read/addtest";
+    static final String REDIRECT_TO_JSON_ALL_LIST = "redirect:/all";
+    static final String LOGIN_PATH = "/login";
+    static final String LOGIN_PAGE = "index";
+    static final String ADMIN_PATH = "/read";
+    static final String ADMIN_PAGE = "loggedin";
+    static final String EXPORT_PATH = "/export";
+    static final String REDIRECT_PDFREPORT = "redirect:/pdfreport";
+    static final String PDFREPORT_PATH = "/pdfreport";
+}
